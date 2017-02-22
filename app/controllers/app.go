99ce2c70613653fa3bca7408c78046307fc0e5ba
zol_zol000@126.com <git007_wdmm>
2017-02-22 11:35:44 +0800

    1

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,11 +1,15 @@
 package controllers
 
-import "github.com/revel/revel"
+import (
+	"github.com/revel/revel"
+	"fmt"
+)
 
 type App struct {
 	*revel.Controller
 }
 
 func (c App) Index() revel.Result {
+	fmt.Printf("test")
 	return c.Render()
 }
